39d4259138a86006f08ebca1f5e555a0852615b5
Jeison Garcia <jt.garciaa@uea.edu.ec>
2026-09-25 20:39:41 -0500

    Proyecto Integrador U4 (Semana 15): CRUD en PostgreSQL

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,11 +1,25 @@
 import psycopg2
 from flask import current_app
 
-def get_db_connection():
-    """Crea y retorna una conexión a la base de datos PostgreSQL."""
 
+def get_db_connection():
+
+    host = current_app.config["POSTGRES_HOST"]
+
+    # PostgreSQL local no necesita SSL.
+    # Neon en Render sí utiliza SSL.
+    if host in ["localhost", "127.0.0.1"]:
+        return psycopg2.connect(
+            host=host,
+            port=current_app.config["POSTGRES_PORT"],
+            dbname=current_app.config["POSTGRES_DB"],
+            user=current_app.config["POSTGRES_USER"],
+            password=current_app.config["POSTGRES_PASSWORD"]
+        )
+
+    # Conexiones externas como Neon utilizan SSL.
     return psycopg2.connect(
-        host=current_app.config["POSTGRES_HOST"],
+        host=host,
         port=current_app.config["POSTGRES_PORT"],
         dbname=current_app.config["POSTGRES_DB"],
         user=current_app.config["POSTGRES_USER"],
